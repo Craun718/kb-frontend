b2340855017f272864376e4f0a8f803a9ac51a46
ProblemPrincipal <3653544699@qq.com>
2025-10-30 03:37:12 +0800

    feat: add search results display and adjust layout

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -13,27 +13,27 @@ export function SearchCard() {
       <CardHeader>
         {/* <CardTitle>搜索</CardTitle> */}
         {/* <CardDescription>请输入您想要的查询内容</CardDescription> */}
-        <CardContent>
-          <Tabs
-            defaultValue="definition"
-            value={isRelation ? 'relation' : 'definition'}
-          >
-            <TabsList>
-              <div className="flex gap-3">
-                <TabsTrigger value="definition">查询定义</TabsTrigger>
-                <Switch onCheckedChange={() => setIsRelation(!isRelation)} />
-                <TabsTrigger value="relation">查询关系</TabsTrigger>
-              </div>
-            </TabsList>
-            <TabsContent value="definition">
-              <DefinitionSingleSearchForm />
-            </TabsContent>
-            <TabsContent value="relation">
-              <RelationSingleSearchForm />
-            </TabsContent>
-          </Tabs>
-        </CardContent>
       </CardHeader>
+      <CardContent>
+        <Tabs
+          defaultValue="definition"
+          value={isRelation ? 'relation' : 'definition'}
+        >
+          <TabsList>
+            <div className="flex gap-3">
+              <TabsTrigger value="definition">查询定义</TabsTrigger>
+              <Switch checked={isRelation} onCheckedChange={() => setIsRelation(!isRelation)} />
+              <TabsTrigger value="relation">查询关系</TabsTrigger>
+            </div>
+          </TabsList>
+          <TabsContent value="definition">
+            <DefinitionSingleSearchForm />
+          </TabsContent>
+          <TabsContent value="relation">
+            <RelationSingleSearchForm />
+          </TabsContent>
+        </Tabs>
+      </CardContent>
     </Card>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
+import { ResultList } from '@/components/resultList';
 import { SearchCard } from '@/components/SearchCard';
+import { resultsListAtom } from '@/store/resultsList';
+import { useAtom } from 'jotai';
 
 export function HomePage() {
+  const resultList = useAtom(resultsListAtom)[0];
+
   return (
-    <>
-      <div className="flex-1">
-        <div style={{ paddingTop: 150 }}>
-          <h1 className="font-bold text-3xl text-center m-10">
-            海洋防灾减灾知识库
-          </h1>
+    <div className="flex-1">
+      <div style={{ paddingTop: resultList.length === 0 ? 180 : 50 }}>
+        <h1 className="font-bold text-3xl text-center m-10">
+          海洋防灾减灾知识库
+        </h1>
+        <div className='flex flex-col gap-15 mx-10 mb-10 lg:mx-70'>
           <SearchCard />
+          <ResultList />
         </div>
       </div>
-    </>
+    </div>
   );
 }
