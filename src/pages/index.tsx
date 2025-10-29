62cb942408dd276bfc7a6a80bd3c68b3400e6b35
ProblemPrincipal <3653544699@qq.com>
2025-10-29 19:05:37 +0800

    chore: adjust title padding and text on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ export function HomePage() {
   return (
     <>
       <div className="flex-1">
-        <div style={{ paddingTop: 250 }}>
+        <div style={{ paddingTop: 150 }}>
           <h1 className="font-bold text-3xl text-center m-10">
-            欢迎来到海洋防灾减灾知识库
+            海洋防灾减灾知识库
           </h1>
           <SearchCard />
         </div>
