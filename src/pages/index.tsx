c11b3f47ab438923b6f7d08fcf14c2ffeb064aae
ProblemPrincipal <3653544699@qq.com>
2025-10-30 10:07:43 +0800

    style(home): adjust padding values for layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export function HomePage() {
 
   return (
     <div className="flex-1">
-      <div style={{ paddingTop: resultList.length === 0 ? 180 : 50 }}>
+      <div style={{ paddingTop: resultList.length === 0 ? 100 : 0 }}>
         <h1 className="font-bold text-3xl text-center m-10">
           海洋防灾减灾知识库
         </h1>
