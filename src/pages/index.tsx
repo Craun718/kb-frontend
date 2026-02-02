35daf10c887c87d4ad48db4b4b20b0007f98906a
ProblemPrincipal <3653544699@qq.com>
2026-02-02 10:19:16 +0800

    docs: update homepage title to natural resource industry standard knowledge base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,8 @@ export function HomePage() {
     <div className="flex-1">
       <div className='pt-10'>
         <h1 className="font-extrabold text-4xl text-center text-sky-900 mb-6">
-          海洋防灾减灾知识库
+          自然资源行业标准知识库
         </h1>
-
         <div className="flex justify-center px-4">
           <div className="w-full max-w-4xl bg-white/70 dark:bg-gray-800/60 backdrop-blur-sm rounded-2xl p-6 shadow-lg">
             <div className="flex flex-col gap-10 mx-1 mt-5 lg:mx-4">
